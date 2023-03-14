56d411894ab5a4aae2cf2fa5705fc085a9be425b
Jean le Grand Bokassa <jean.le.grand.bokassa@gmail.com>
2023-03-14 09:59:06 +0100

    added convert algo

---
+++ convert.php
@@ -0,0 +1,29 @@
+<?php
+// Ici ton php
+?>
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Convert</title>
+</head>
+<body>
+<h1>Convert</h1>
+<p>
+</p>
+Ecrire une function ( ou programme ) qui convertit une lettre en une autre et renvoie le résultat:
+<ul>
+    <li>La lettre "A" en "U"</li>
+    <li> La lettre "T" en "D"</li>
+    <li>La lettre "X" en "P"</li>
+    <li>La lettre "K" en "C"</li>
+</ul>
+<p>
+    Dans tous les autres cas, renvoyer l'élément initial.
+    Si l'élément reçu n'est pas un caractère alphanumérique, renvoyer "error".
+</p>
+</body>
+</html>
